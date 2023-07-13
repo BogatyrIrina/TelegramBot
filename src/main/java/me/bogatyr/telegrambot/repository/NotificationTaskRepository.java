97b536f14b52ea9ae00dee4f90676c105cf7e241
admin <kochereva_irina@mail.ru>
2023-07-14 00:54:12 +0300

    Телеграм-Бот

--- src/main/java/me/bogatyr/telegrambot/repository/NotificationTaskRepository.java
+++ src/main/java/me/bogatyr/telegrambot/repository/NotificationTaskRepository.java
@@ -1,2 +1,13 @@
-package me.bogatyr.telegrambot.repository;public class NotificationTaskRepository {
+package me.bogatyr.telegrambot.repository;
+
+import me.bogatyr.telegrambot.entity.NotificationTask;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
+    List<NotificationTask> findAllByNotificationDateTime(LocalDateTime localDateTime);
+    List<NotificationTask> findAllByNotificationDateTimeAndChatId(LocalDateTime localDateTime,
+                                                                  long chatID);
 }
--- src/main/java/me/bogatyr/telegrambot/service/NotificationTaskService.java
+++ src/main/java/me/bogatyr/telegrambot/service/NotificationTaskService.java
@@ -1,2 +1,17 @@
-package me.bogatyr.telegrambot.service;public class NotificationTaskService {
+package me.bogatyr.telegrambot.service;
+
+import me.bogatyr.telegrambot.entity.NotificationTask;
+import me.bogatyr.telegrambot.repository.NotificationTaskRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationTaskService {
+    private final NotificationTaskRepository notificationTaskRepository;
+
+    public NotificationTaskService(NotificationTaskRepository notificationTaskRepository) {
+        this.notificationTaskRepository = notificationTaskRepository;
+    }
+    public void save(NotificationTask notificationTask){
+        notificationTaskRepository.save(notificationTask);
+    }
 }
