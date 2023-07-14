b3feb2c8596bda379ae340b42138ad49d13056f7
admin <kochereva_irina@mail.ru>
2023-07-14 14:15:43 +0300

    Телеграм-Бот доп

--- src/main/java/me/bogatyr/telegrambot/repository/NotificationTaskRepository.java
+++ src/main/java/me/bogatyr/telegrambot/repository/NotificationTaskRepository.java
@@ -2,6 +2,9 @@ package me.bogatyr.telegrambot.repository;
 
 import me.bogatyr.telegrambot.entity.NotificationTask;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,4 +13,7 @@ public interface NotificationTaskRepository extends JpaRepository<NotificationTa
     List<NotificationTask> findAllByNotificationDateTime(LocalDateTime localDateTime);
     List<NotificationTask> findAllByNotificationDateTimeAndChatId(LocalDateTime localDateTime,
                                                                   long chatID);
+    @Modifying
+    @Query("DELETE FROM NotificationTask WHERE message like %:nameLike%")
+    void removeAllLike(@Param("nameLike") String nameLike);
 }
