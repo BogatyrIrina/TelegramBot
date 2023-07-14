dd8fdd5748d19fbf7e6e8626cfa320fb8553042a
admin <kochereva_irina@mail.ru>
2023-07-14 14:47:16 +0300

    Телеграм-Бот завершен

--- src/main/java/me/bogatyr/telegrambot/config/TelegramBotConfiguration.java
+++ src/main/java/me/bogatyr/telegrambot/config/TelegramBotConfiguration.java
@@ -1,15 +1,20 @@
 package me.bogatyr.telegrambot.config;
 
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.DeleteMyCommands;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TelegramBotConfiguration {
+    @Value("${telegram.bot.token}")
+    private String token;
 
     @Bean
-    public TelegramBot telegramBot(@Value("${telegram.bot.token}") String token){
-        return new TelegramBot(token);
+    public TelegramBot telegramBot() {
+        TelegramBot bot = new TelegramBot(token);
+        bot.execute(new DeleteMyCommands());
+        return bot;
     }
 }
